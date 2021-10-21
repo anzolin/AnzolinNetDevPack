46980cd4b0d5822c29831e664e313c3915537951
Diego <diego@anzolin.com.br>
2021-10-21 14:54:24 -0300

    Versáo 1.0.5

---
+++ source/AnzolinNetDevPack/CustomAttributes/BackgroundStyleAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AnzolinNetDevPack.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class BackgroundStyleAttribute : Attribute
+    {
+        private readonly string backgroundColorClass;
+
+        public BackgroundStyleAttribute(string backgroundColorClass)
+        {
+            this.backgroundColorClass = backgroundColorClass;
+        }
+
+        public virtual string BackgroundColorClass
+        {
+            get { return backgroundColorClass; }
+        }
+    }
+}
---
+++ source/AnzolinNetDevPack/CustomAttributes/BadgeStyleAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AnzolinNetDevPack.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class BadgeStyleAttribute : Attribute
+    {
+        private readonly string textColorClass;
+        private readonly string backgroundColorClass;
+
+        public BadgeStyleAttribute(string textColorClass, string backgroundColorClass)
+        {
+            this.textColorClass = textColorClass;
+            this.backgroundColorClass = backgroundColorClass;
+        }
+
+        public virtual string TextColorClass
+        {
+            get { return textColorClass; }
+        }
+
+        public virtual string BackgroundColorClass
+        {
+            get { return backgroundColorClass; }
+        }
+    }
+}
