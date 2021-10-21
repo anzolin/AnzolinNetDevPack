d5ea550ff590f0ac9c77ccfa7916a689cb350479
Diego <diego@anzolin.com.br>
2021-10-21 15:03:51 -0300

    Versão 1.0.6

---
+++ source/AnzolinNetDevPack/Helpers/EnumBackgroundStyleHelper.cs
@@ -0,0 +1,41 @@
+﻿using AnzolinNetDevPack.CustomAttributes;
+using System;
+
+namespace AnzolinNetDevPack.Helpers
+{
+    public static class EnumBackgroundStyleHelper
+    {
+        public static string GetBackgroundStyle(object aEnum)
+        {
+            if (((object)aEnum) == null) throw new ArgumentNullException("aEnum");
+
+            var vEnumType = aEnum.GetType();
+
+            if (!vEnumType.IsEnum)
+                throw new Exception("Object is not an Enum");
+
+            var badgeStyleAttribute = (BackgroundStyleAttribute)Attribute.GetCustomAttribute(vEnumType, typeof(BackgroundStyleAttribute));
+
+            return badgeStyleAttribute.BackgroundColorClass;
+        }
+
+        public static string GetBackgroundStyle(Type type)
+        {
+            var badgeStyleAttribute = (BackgroundStyleAttribute)Attribute.GetCustomAttribute(type, typeof(BackgroundStyleAttribute));
+
+            return badgeStyleAttribute.BackgroundColorClass;
+        }
+
+        public static string GetBackgroundStyle(Type type, int key)
+        {
+            if (!type.IsEnum)
+                throw new Exception("Type must be an Enum type");
+
+            var vEnumName = type.GetEnumName(key);
+            var vMemberInfo = type.GetMember(vEnumName);
+            var vAttributes = (BackgroundStyleAttribute)vMemberInfo[0].GetCustomAttributes(typeof(BackgroundStyleAttribute), false)[0];
+
+            return vAttributes.BackgroundColorClass;
+        }
+    }
+}
---
+++ source/AnzolinNetDevPack/Helpers/EnumBadgeStyleHelper.cs
@@ -0,0 +1,70 @@
+﻿using AnzolinNetDevPack.CustomAttributes;
+using System;
+
+namespace AnzolinNetDevPack.Helpers
+{
+    public static class EnumBadgeStyleHelper
+    {
+        public static string GetBadge(object aEnum)
+        {
+            if (((object)aEnum) == null) throw new ArgumentNullException("aEnum");
+
+            var vEnumType = aEnum.GetType();
+
+            if (!vEnumType.IsEnum)
+                throw new Exception("Object is not an Enum");
+
+            var label = EnumHelper.GetText(vEnumType, aEnum.ToString());
+            var badgeStyleAttribute = (BadgeStyleAttribute)Attribute.GetCustomAttribute(vEnumType, typeof(BadgeStyleAttribute));
+
+            return $"<div class='badge badge-pill {badgeStyleAttribute.BackgroundColorClass}'>{label}</div>";
+        }
+
+        public static string GetBadge(Type type)
+        {
+            var label = EnumHelper.GetText(type);
+            var badgeStyleAttribute = (BadgeStyleAttribute)Attribute.GetCustomAttribute(type, typeof(BadgeStyleAttribute));
+
+            return $"<div class='badge badge-pill {badgeStyleAttribute.BackgroundColorClass}'>{label}</div>";
+        }
+
+        public static string GetBadge(Type type, int key)
+        {
+            if (!type.IsEnum)
+                throw new Exception("Type must be an Enum type");
+
+            var vEnumName = type.GetEnumName(key);
+            var label = EnumHelper.GetText(type, vEnumName);
+            var vMemberInfo = type.GetMember(vEnumName);
+            var vAttributes = (BadgeStyleAttribute)vMemberInfo[0].GetCustomAttributes(typeof(BadgeStyleAttribute), false)[0];
+
+            return $"<div class='badge badge-pill {vAttributes.BackgroundColorClass}'>{label}</div>";
+        }
+
+        public static string GetBadgeTextColorClass(Type type, int key)
+        {
+            if (!type.IsEnum)
+                throw new Exception("Type must be an Enum type");
+
+            var vEnumName = type.GetEnumName(key);
+            var label = EnumHelper.GetText(type, vEnumName);
+            var vMemberInfo = type.GetMember(vEnumName);
+            var vAttributes = (BadgeStyleAttribute)vMemberInfo[0].GetCustomAttributes(typeof(BadgeStyleAttribute), false)[0];
+
+            return vAttributes.TextColorClass;
+        }
+
+        public static string GetBadgeBackgroundColorClass(Type type, int key)
+        {
+            if (!type.IsEnum)
+                throw new Exception("Type must be an Enum type");
+
+            var vEnumName = type.GetEnumName(key);
+            var label = EnumHelper.GetText(type, vEnumName);
+            var vMemberInfo = type.GetMember(vEnumName);
+            var vAttributes = (BadgeStyleAttribute)vMemberInfo[0].GetCustomAttributes(typeof(BadgeStyleAttribute), false)[0];
+
+            return vAttributes.BackgroundColorClass;
+        }
+    }
+}
