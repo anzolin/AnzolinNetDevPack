c9af098c8719c23cf1357a61b4e30ae9e148d5da
Diego <diego@anzolin.com.br>
2021-11-03 20:00:31 -0300

    Versão 1.1.0

---
+++ source/AnzolinNetDevPack/Helpers/EncryptionHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AnzolinNetDevPack.Helpers
+{
+    public static class EncryptionHelper
+    {
+        private const string ENCRYPTION_KEY = "abracadabra pé de cabra";
+
+        /// <summary>
+        /// Encripta e retorna o valor informado para a string.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string Encrypt(string value)
+        {
+            byte[] clearBytes = Encoding.Unicode.GetBytes(value);
+
+            using (Aes encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(ENCRYPTION_KEY, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+
+                    value = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Desencripta e retorna o valor informado para a string.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string Decrypt(string value)
+        {
+            value = value.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(value);
+
+            using (Aes encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(ENCRYPTION_KEY, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+
+                    value = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+
+            return value;
+        }
+    }
+}
