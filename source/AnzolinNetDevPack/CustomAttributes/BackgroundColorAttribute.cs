ef48edc2f5da7a599bf6acd0d76977743360d7ea
Diego <diego@anzolin.com.br>
2022-09-22 10:14:37 -0300

    Versão 1.3.0

---
+++ source/AnzolinNetDevPack/CustomAttributes/BackgroundColorAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AnzolinNetDevPack.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class BackgroundColorAttribute : Attribute
+    {
+        private readonly string _backgroundColorHexCode;
+
+        public BackgroundColorAttribute(string backgroundColorHexCode)
+        {
+            _backgroundColorHexCode = backgroundColorHexCode;
+        }
+
+        public virtual string BackgroundColorHexCode
+        {
+            get { return _backgroundColorHexCode; }
+        }
+    }
+}
---
+++ source/AnzolinNetDevPack/Helpers/EnumBackgroundColorHelper.cs
@@ -0,0 +1,41 @@
+﻿using AnzolinNetDevPack.CustomAttributes;
+using System;
+
+namespace AnzolinNetDevPack.Helpers
+{
+    public static class EnumBackgroundColorHelper
+    {
+        public static string GetBackgroundColor(object aEnum)
+        {
+            if (((object)aEnum) == null) throw new ArgumentNullException("aEnum");
+
+            var vEnumType = aEnum.GetType();
+
+            if (!vEnumType.IsEnum)
+                throw new Exception("Object is not an Enum");
+
+            var colorAttribute = (BackgroundColorAttribute)Attribute.GetCustomAttribute(vEnumType, typeof(BackgroundColorAttribute));
+
+            return colorAttribute.BackgroundColorHexCode;
+        }
+
+        public static string GetBackgroundColor(Type type)
+        {
+            var colorAttribute = (BackgroundColorAttribute)Attribute.GetCustomAttribute(type, typeof(BackgroundColorAttribute));
+
+            return colorAttribute.BackgroundColorHexCode;
+        }
+
+        public static string GetBackgroundColor(Type type, int key)
+        {
+            if (!type.IsEnum)
+                throw new Exception("Type must be an Enum type");
+
+            var vEnumName = type.GetEnumName(key);
+            var vMemberInfo = type.GetMember(vEnumName);
+            var vAttributes = (BackgroundColorAttribute)vMemberInfo[0].GetCustomAttributes(typeof(BackgroundColorAttribute), false)[0];
+
+            return vAttributes.BackgroundColorHexCode;
+        }
+    }
+}
